636c2ddac8da5a8517a6bcd661851deb621f11f8
YJ Soon <yjsoon@tinkertanker.com>
2022-07-09 10:10:39 +0800

    Todo now conforms to Codable

--- Todos v3/Todo.swift
+++ Todos v3/Todo.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-struct Todo: Identifiable {
+struct Todo: Identifiable, Codable {
     
     var id = UUID()
     var title: String
-    var isCompleted = false 
+    var isCompleted = false
     
 }
